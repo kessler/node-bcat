5b0283467b60fe6026b2445807db74f67921e735
nanohertz <dev@nhz.io>
2022-10-13 20:28:22 +0300

    fix indentation (back to tabs)

--- index.js
+++ index.js
@@ -137,10 +137,10 @@ function handler(request, response) {
 			'.noselect { ' +
 			'-webkit-touch-callout: none; /* iOS Safari */ ' +
 			'-webkit-user-select: none; /* Safari */ ' +
-      '-khtml-user-select: none; /* Konqueror HTML */ ' +
-      '-moz-user-select: none; /* Old versions of Firefox */ ' +
-      '-ms-user-select: none; /* Internet Explorer/Edge */ ' +
-      'user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */ }'
+			'-khtml-user-select: none; /* Konqueror HTML */ ' +
+			'-moz-user-select: none; /* Old versions of Firefox */ ' +
+			'-ms-user-select: none; /* Internet Explorer/Edge */ ' +
+			'user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */ }'
 
 		response.write('<!DOCTYPE html><html><head>' +
 			'<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">' +
